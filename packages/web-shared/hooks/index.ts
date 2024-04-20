96651c9e193cf6a9e639f1dace1f8d57384a4340
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-04-20 18:47:46 +0300

    Update index.ts

--- packages/web-shared/hooks/index.ts
+++ packages/web-shared/hooks/index.ts
@@ -1,2 +1,7 @@
 export * from "./use-event-outside-element";
 export * from "./use-form";
+export * from "./use-hover";
+export * from "./use-page-bottom";
+export * from "./use-element-bottom";
+export * from "./use-element-on-screen";
+export * from "./use-debounce";
