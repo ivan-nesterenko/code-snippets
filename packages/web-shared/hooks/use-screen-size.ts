3a9e8a5a9142f6faff93803824c745724927d168
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-06-29 00:39:57 +0300

    Create use-screen-size.ts

---
+++ packages/web-shared/hooks/use-screen-size.ts
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+export const useScreenSize = (
+  callback?: (props: { width: number; height: number }) => void,
+) => {
+  const [screenSize, setScreenSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+      callback?.({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [callback]);
+
+  return screenSize;
+};
