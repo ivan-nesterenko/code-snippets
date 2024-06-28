492e5e7baf9a7107c0af9fec1f081cdcbf966e36
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-06-29 00:17:20 +0300

    Added modal

---
+++ packages/web-shared/hooks/modal-wrapper.tsx
@@ -0,0 +1,86 @@
+import { type FC, type ReactNode, memo, useCallback } from "react";
+
+import { twMerge } from "tailwind-merge";
+import {
+  type OptionalStyleProps,
+  useOptionalStyle,
+  type OptionalCustomStylePropsVariant,
+  type OptionalDefaultStylePropsVariant,
+} from "../use-optional-style";
+
+enum ModalWrapperPositionVariant {
+  CENTRED = "justify-center items-center",
+  NONE = "",
+}
+
+enum ModalWrapperMode {
+  BLUR = "backdrop-blur-sm",
+  SHADING = "backdrop-brightness-50",
+  NONE = "",
+}
+
+type BgAnimationProps = (
+  | OptionalCustomStylePropsVariant
+  | OptionalDefaultStylePropsVariant
+) &
+  Except<OptionalStyleProps, "initialEnabled">;
+
+export type ModalVariantProps = {
+  position?: keyof typeof ModalWrapperPositionVariant;
+  mode?: keyof typeof ModalWrapperMode;
+  onBgClick?: () => void;
+  className?: string;
+  bgAnimationProps?: BgAnimationProps;
+};
+
+export type ModalWrapperProps = {
+  children: ReactNode;
+} & ModalVariantProps;
+
+export const ModalWrapper: FC<ModalWrapperProps> = memo(
+  ({
+    children,
+    className,
+    position = "CENTRED",
+    mode = "SHADING",
+    onBgClick,
+    bgAnimationProps = {
+      animationStyle: "FADE" as const,
+      timing: "MEDIUM" as const,
+    },
+  }) => {
+    const { disableStyle: disableWrapperStyle, ref } =
+      useOptionalStyle(bgAnimationProps);
+
+    const handleBgClick = useCallback(() => {
+      disableWrapperStyle();
+      onBgClick?.();
+    }, [disableWrapperStyle, onBgClick]);
+
+    return (
+      <div
+        className={twMerge(
+          "absolute left-0 top-0 z-50 flex h-screen w-screen overflow-hidden",
+        )}
+      >
+        <div
+          className={twMerge(
+            "relative z-50 h-fit w-fit transition-all",
+            ModalWrapperPositionVariant[position],
+          )}
+        >
+          {children}
+        </div>
+        <div
+          ref={ref}
+          className={twMerge(
+            "absolute h-full w-full",
+            ModalWrapperMode[mode],
+            className,
+          )}
+          onClick={handleBgClick}
+        />
+      </div>
+    );
+  },
+);
---
+++ packages/web-shared/hooks/use-modal.tsx
@@ -0,0 +1,122 @@
+import {
+  useState,
+  type FC,
+  type ReactNode,
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+  memo,
+} from "react";
+import {
+  type ModalVariantProps,
+  ModalWrapper,
+  type ModalWrapperProps,
+} from "./modal-wrapper";
+
+type ModalProviderProps = {
+  children: ReactNode;
+};
+
+type ModalParamsProps = {
+  showCascadingModels?: boolean;
+} & Partial<ModalVariantProps>;
+
+type ModalProperties = {
+  content: ReactNode;
+  params?: ModalParamsProps;
+};
+
+type ModalContextProperties = {
+  modals: ModalProperties[];
+  openModalWithHistory: (content: ReactNode, params?: ModalParamsProps) => void;
+  openModalWithoutHistory: (
+    content: ReactNode,
+    params?: ModalParamsProps,
+  ) => void;
+  closeModal: () => void;
+  closeAllModals: () => void;
+  ModalWrapper: FC<ModalWrapperProps>;
+};
+
+const modalContext = createContext<ModalContextProperties>({
+  modals: [{ content: <></>, params: { showCascadingModels: false } }],
+  openModalWithHistory: () => "",
+  openModalWithoutHistory: () => "",
+  closeModal: () => "",
+  closeAllModals: () => "",
+  ModalWrapper,
+});
+
+type ModalListProps = {
+  modalsData: ModalProperties[];
+  cascadeRender: boolean;
+};
+
+const ModalList: FC<ModalListProps> = memo(({ modalsData, cascadeRender }) =>
+  cascadeRender ? (
+    modalsData?.map(({ content }, i) => (
+      <div key={`${i}_${new Date().getUTCDate()}`}>{content}</div>
+    ))
+  ) : (
+    <div>{modalsData[modalsData?.length - 1]?.content}</div>
+  ),
+);
+
+export const ModalProvider: FC<ModalProviderProps> = ({ children }) => {
+  const [modals, setModals] = useState<ModalProperties[]>([]);
+
+  const openModalWithHistory = useCallback(
+    (content: ReactNode, params?: Partial<ModalVariantProps>) =>
+      setModals((prevProps) => [...prevProps, { content: content, ...params }]),
+    [],
+  );
+
+  const openModalWithoutHistory = useCallback(
+    (content: ReactNode, params?: Partial<ModalVariantProps>) =>
+      setModals(() => [{ content: content, ...params }]),
+    [],
+  );
+
+  const closeModal = useCallback(
+    () => setModals((prevProps) => prevProps.slice(0, prevProps.length - 1)),
+    [],
+  );
+
+  const closeAllModals = useCallback(() => setModals([]), []);
+
+  const currentModal = useMemo(() => modals[modals.length - 1], [modals]);
+
+  const modalContextValue: ModalContextProperties = useMemo(
+    () => ({
+      openModalWithHistory,
+      closeModal,
+      closeAllModals,
+      openModalWithoutHistory,
+      modals,
+      ModalWrapper,
+    }),
+    [
+      closeAllModals,
+      closeModal,
+      modals,
+      openModalWithHistory,
+      openModalWithoutHistory,
+    ],
+  );
+
+  return (
+    <modalContext.Provider value={modalContextValue}>
+      {modals.length > 0 && (
+        <ModalList
+          modalsData={modals}
+          cascadeRender={!!currentModal?.params?.showCascadingModels}
+        />
+      )}
+
+      {children}
+    </modalContext.Provider>
+  );
+};
+
+export const useOverlay = () => useContext(modalContext);
