cdca573c40b6ecc76860d9c06a209fbf4e63f663
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-07-06 17:32:27 +0300

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,12 +1,29 @@
 declare global {
   type Except<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
   type DeepPartial<T> = T extends object
     ? {
         [P in keyof T]?: DeepPartial<T[P]>;
       }
     : T;
-  
-    interface ObjectConstructor {
+
+  type DeepRequired<T> = T extends object
+    ? {
+        [P in keyof T]-?: DeepRequired<T[P]>;
+      }
+    : T;
+
+  interface String {
+    toLowerCase(): Lowercase<string>;
+
+    toLocaleLowerCase(locales?: string | string[]): Lowercase<string>;
+
+    toUpperCase(): Uppercase<string>;
+
+    toLocaleUpperCase(locales?: string | string[]): Uppercase<string>;
+  }
+
+  interface ObjectConstructor {
     keys<T extends object>(o: T): (keyof T)[];
 
     entries<TValue, TKey>(
