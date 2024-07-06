cbab91a78783bb4c100c6be20831f2ded189cc2a
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-07-06 17:38:37 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,39 +15,50 @@ const config = {
       },
     },
     extend: {
-      // cubic-bezier(0.68, -0.55, 0.265, 1.55)
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        
-        fadeIn: "fadeIn .1s ease-in-out",
-        fadeOut: "fadeOut .1s ease-in-out",
+     // cubic-bezier(0.42, 0, 0.58, 1)  ----  Ease In Out
+    // cubic-bezier(0.42, 0, 1, 1)  ----  Ease In
+    // cubic-bezier(0, 0, 0.58, 1)  ----  Ease Out
+    // cubic-bezier(0.25, 0.1, 0.25, 1)  ----  Ease
+    // cubic-bezier(0.47, 0, 0.745, 0.715)  ----  Ease In Sine
+    // cubic-bezier(0.39, 0.575, 0.565, 1)  ----  Ease Out Sine
+    // cubic-bezier(0.445, 0.05, 0.55, 0.95)  ----  Ease In Out Sine
+    // cubic-bezier(0.55, 0.085, 0.68, 0.53)  ----  Ease In Quad
+    // cubic-bezier(0.25, 0.46, 0.45, 0.94)  ----  Ease Out Quad
+    // cubic-bezier(0.455, 0.03, 0.515, 0.955)  ----  Ease In Out Quad
 
-        slideInLeft: "slideInLeft 0.3s ease-in-out",
-        slideOutLeft: "slideOutLeft 0.3s ease-in-out",
+    animation: {
+      accordionDown: "accordion-down 0.2s ease-out",
+      accordionUp: "accordion-up 0.2s ease-out",
 
-        slideInRight: "slideInRight 0.3s ease-in-out",
-        slideOutRight: "slideOutRight 0.3s ease-in-out",
+      fadeIn: "fadeIn .1s ease-in-out",
+      fadeOut: "fadeOut .1s ease-in-out",
 
-        slideInUp: "slideInUp 0.3s ease-in-out",
-        slideOutUp: "slideOutUp 0.3s ease-in-out",
+      slideInLeft: "slideInLeft .3s ease-in-out",
+      slideOutLeft: "slideOutLeft .3s ease-in-out",
 
-        slideInDown: "slideInDown 0.3s ease-in-out",
-        slideOutDown: "slideOutDown 0.3s ease-in-out",
+      slideInRight: "slideInRight 0.3s ease-in-out",
+      slideOutRight: "slideOutRight 0.3s ease-in-out",
 
-        scaleIn: "scaleIn 0.3s ease-in-out",
-        scaleOut: "scaleOut 0.3s ease-in-out",
+      slideInUp: "slideInUp 0.3s ease-in-out",
+      slideOutUp: "slideOutUp 0.3s ease-in-out",
+
+      slideInDown: "slideInDown 0.3s ease-in-out",
+      slideOutDown: "slideOutDown 0.3s ease-in-out",
+
+      scaleIn: "scaleIn 0.3s ease-in-out",
+      scaleOut: "scaleOut 0.3s ease-in-out",
+    },
+    keyframes: {
+      accordionDown: {
+        from: { height: "0" },
+        to: { height: "var(--radix-accordion-content-height)" },
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-              fadeIn: {
+      accordionUp: {
+        from: { height: "var(--radix-accordion-content-height)" },
+        to: { height: "0" },
+      },
+
+      fadeIn: {
         from: { opacity: "0" },
         to: { opacity: "1" },
       },
@@ -100,7 +111,7 @@ const config = {
         from: { transform: "scale(1)" },
         to: { transform: "scale(0.5)" },
       },
-      },
+    },
     },
   },
 } satisfies Config;
